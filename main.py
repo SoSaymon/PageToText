f766ca817785b7c0a1c038298b666d241eb3c423
SoSaymon <szymon.chirowski@protonmail.com>
2024-02-17 01:30:15 +0100

    feat: add base PDF to text converter

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import List
 from bs4 import BeautifulSoup
 import ebooklib
 from ebooklib import epub
+from spire.pdf import PdfDocument
 
 
 def epub2html(epub_path: str) -> List:
@@ -47,8 +48,29 @@ def output_text(text):
             f.write(t)
 
 
+def pdf2list(pdf_path):
+    doc = PdfDocument()
+    doc.LoadFromFile(pdf_path)
+
+    list_ = []
+
+    for i in range(doc.Pages.Count):
+        page = doc.Pages.get_Item(i)
+        text = page.ExtractText(True)
+        list_.append(text)
+
+    doc.Close()
+
+    return list_
+
+
+def list2text(list_):
+    with open("output.txt", "w", encoding="utf-8") as f:
+        for text in list_:
+            f.write(text + "\n")
+
+
 if __name__ == "__main__":
-    path = "ebook.epub"
     blacklist = [
         "[document]",
         "noscript",
@@ -60,5 +82,15 @@ if __name__ == "__main__":
         "script",
     ]
 
-    out = epub2text(path)
-    output_text(out)
+    choice = input("Do you want to convert PDF or EPUB? (pdf/epub): ")
+
+    if choice == "pdf":
+        path = "ebook.pdf"
+        out = pdf2list(path)
+        list2text(out)
+    elif choice == "epub":
+        path = "ebook.epub"
+        out = epub2text(path)
+        output_text(out)
+    else:
+        print("Invalid choice")
